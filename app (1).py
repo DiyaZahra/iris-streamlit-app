06aa5e5d01fd40009d1baa0e033cbefd8a061b78
DiyaZahra <diya90x@gmail.com>
2026-09-18 22:54:41 +0500

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,21 @@
+import streamlit as st
+import numpy as np
+import joblib
+from tensorflow import keras
+
+model = keras.models.load_model('model.keras')
+scaler = joblib.load('scaler.pkl')
+
+st.title("Iris Species Predictor")
+
+sepal_length = st.number_input("Sepal Length")
+sepal_width = st.number_input("Sepal Width")
+petal_length = st.number_input("Petal Length")
+petal_width = st.number_input("Petal Width")
+
+if st.button("Predict"):
+    features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    features_scaled = standard_scaler.transform(features)
+    prediction = model.predict(features_scaled)
+    species = ['Setosa', 'Versicolor', 'Virginica']
+    st.success(f"Predicted Species: {species[np.argmax(prediction)]}")
